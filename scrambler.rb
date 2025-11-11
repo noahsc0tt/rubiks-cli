146619e9ac2abc1753d46cf8bd050721915117b3
Noah Scott <noah.scott.24@ucl.ac.uk>
2025-11-11 21:59:34 +0000

    adapted c# scrambler (from interactive-rubiks-solver) for ruby

---
+++ scrambler.rb
@@ -0,0 +1,29 @@
+module Scrambler
+  LENGTH = 20
+  FACES = ['U', 'D', 'F', 'B', 'L', 'R']
+  SUFFIXES = [ '', '\'', '2' ]
+
+  def self.get_scramble
+    moves = []
+    second_to_last_face = ''
+    last_face = ''
+
+    LENGTH.times do |i|
+        next_move = ""
+        next_face = ''
+        begin
+            next_move = self.get_move
+            next_face = next_move[0]
+        end while next_face == second_to_last_face || next_face == last_face
+
+        moves[i] = next_move
+        second_to_last_face = last_face
+        last_face = next_face
+    end
+    moves.join(", ")
+  end
+
+  def self.get_move
+    FACES.sample + SUFFIXES.sample
+  end
+end
