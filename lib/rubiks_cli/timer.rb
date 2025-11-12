495ab1f955bfcb1a75e9048a7e62660ec86ff595
Noah Scott <noah.scott.24@ucl.ac.uk>
2025-11-12 22:57:07 +0000

    janky inspection timer

--- lib/rubiks_cli/timer.rb
+++ lib/rubiks_cli/timer.rb
@@ -7,6 +7,7 @@ module RubiksCli
       puts "\r#{self.time.round(3)}"
     end
 
+    
     def self.time
       start = Time.now
 
@@ -20,5 +21,30 @@ module RubiksCli
 
       return Time.now - start
     end
+
+    def self.inspection
+      STDIN.raw do
+        start = Time.now
+        while (i = (Time.now - start).floor()) < 5
+          print "\r#{i}"
+          sleep(0.01)
+          return if STDIN.wait_readable(0.01)
+        end
+        print "\r+2"
+        start = Time.now
+        while (Time.now - start) < 2
+          return if STDIN.wait_readable(0.01)
+        end
+        print "\rDNF"
+
+        STDIN.read(1) rescue nil
+      ensure
+        STDIN.read_nonblock(1) rescue nil
+      end
+    end
   end
+end
+
+if __FILE__ == $0
+  RubiksCli::Timer.inspection
 end
