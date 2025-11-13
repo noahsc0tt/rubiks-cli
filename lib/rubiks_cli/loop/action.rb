06f94e7c7aaec0a252391682651c1b6db37bfdd1
Noah Scott <noah.scott.24@ucl.ac.uk>
2025-11-13 02:15:10 +0000

    default loop actions

--- lib/rubiks_cli/loop/action.rb
+++ lib/rubiks_cli/loop/action.rb
@@ -4,10 +4,14 @@ module RubiksCli
       private_class_method :new
     end
 
+    DEFAULT_ACTIONS = [
+      build_action('c', Clear.method(:screen), 'clear the screen'),
+      build_action('q', lambda { raise SystemExit }, 'quit'),
+    ]
+
     def self.build_action(command, function, description)
         help = "Press '#{command}' to #{description}"
         Action.send(:new, command, function, help)
     end
-
   end
 end
