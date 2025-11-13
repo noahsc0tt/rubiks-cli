d3fb9f116f3544e904a3a1798b42166cbc2c1749
Noah Scott <noah.scott.24@ucl.ac.uk>
2025-11-13 03:58:07 +0000

    input clearing in loop

--- lib/rubiks_cli/loop/engine.rb
+++ lib/rubiks_cli/loop/engine.rb
@@ -1,22 +1,26 @@
+require_relative '../clear'
+
 module RubiksCli
   module Loop
     class Engine
       private_class_method :new
 
-      def initialize(on_enter, actions)
-        commands = actions.map { |action| action.command }
+      def initialize(action_on_enter, other_actions)
+        commands = other_actions.map { |action| action.command }
         if commands.uniq.size != commands.size
           raise ArgumentError, "Duplicate command"
         end
 
-        @actions = Hash.new(on_enter)
-        actions.each { |action| @actions[action.command] = action.function }
+        @actions = Hash.new(action_on_enter.function)
+        other_actions.each { |action| @actions[action.command] = action.function }
       end
 
       def start
         @actions['h'].call
         loop do
-          @actions[gets.chomp.downcase].call
+          input = gets.chomp.downcase
+          Clear.line_above
+          @actions[input].call
         end
       end
     end
