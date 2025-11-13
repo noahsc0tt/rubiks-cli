902c64929e645cb483d23c848e70fb692168a825
Noah Scott <noah.scott.24@ucl.ac.uk>
2025-11-13 02:30:18 +0000

    build_loop method

---
+++ lib/rubiks_cli/loop/builder.rb
@@ -0,0 +1,12 @@
+module RubiksCli
+  module Loop
+    def self.build_help_message(actions)
+      actions.map { |action| action.help }.join("\n")
+    end
+
+    def self.build_loop(actions)
+      help_action = build_action('h', -> { puts build_help_message(actions) }, "show help")
+      Loop::Loop.send(:new, actions += DEFAULT_ACTIONS << help_action)
+    end
+  end
+end
